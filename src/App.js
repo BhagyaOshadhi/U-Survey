5bdc9778922c48740a4a673164519e15f28495bf
Bhagya <bhagya@fot.ruh.ac.lk>
2021-01-17 23:05:37 +0530

    1st chapter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
-import Usurvey from './FireBase/Usurvey';
-
+import { Home } from './components/Home';
+import { AddUser } from './components/AddUser';
+import { EditUser } from './components/EditUser';
+import {BrowserRouter as Router ,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <p>Welcome  </p>
-     <Usurvey/>
+    <div style={{maxWidth:"30rem",margin:"4rem auto"}}>
+      <Router>
+        <Switch>
+            <AddUser path="/add" component={AddUser}/>
+            <EditUser path="/edit/:id" component={EditUser}/>
+            <Home path="/" component={Home}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
---
+++ src/components/AddUser.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Form,FormGroup,Label,Input,Button} from 'reactstrap';
+import {Link} from 'react-router-dom'
+
+export const AddUser = () =>{
+    return(
+      <Form>
+          <FormGroup>
+              <Label>Name</Label>
+              <Input type="text" placeholder="Enter Name"></Input>
+          </FormGroup>
+          <Button type="submit">Submit</Button>
+          <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
+      </Form>
+    )
+}
