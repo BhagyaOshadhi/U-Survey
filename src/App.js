302913ce72fda50a61605213845c6d98b55e7c71
Bhagya <bhagya@fot.ruh.ac.lk>
2021-01-30 14:49:52 +0530

    asd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+import React from 'react';
 import './App.css';
-import Usurvey from './FireBase/Usurvey';
+import {Cards,Charts,CountryPicker} from './components';
+import {fetchData} from './API/index'
+import axios from 'axios';  
+ 
+class App extends React.Component{
+  state = {
+    data: {},
+    country:"",
+    url : 'https://covid19.mathdro.id/api',
+  }
+  
 
 
-function App() {
-  return (
-    <div className="App">
-     <p>Welcome  </p>
-     <Usurvey/>
-    </div>
+  
+  fetchData = async(country) =>{
+      let changableUrl = this.state.url;
+      if (country){
+         changableUrl = `${this.state.url}/countries/${country}`
+      }
+      try{
+       const {data : {confirmed,recovered,deaths,lastUpdate}} = await axios.get(changableUrl);
+       console.log({confirmed,recovered,deaths,lastUpdate})
+      //  this.setState({data:data})
+       return { confirmed,recovered,deaths,lastUpdate,};
+      }catch(error){
+       console.log(error)
+    }
+   }
+
+
+ 
+  render(){
+    const {data,country} = this.state;
+    return (
+      <div className="container">
+          <Cards data={this.fetchData}/>
+          <CountryPicker  data={data} handleCountryChange={this.handleCountryChange}/>
+          <Charts data={data} country={country}/>
+      </div>
   );
+}
 }
 
 export default App;
