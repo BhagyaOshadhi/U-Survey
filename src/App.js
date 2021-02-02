ec0c0f7f99a7a905ec7baa47ea1dc540bab8a3d2
Bhagya <bhagya@fot.ruh.ac.lk>
2021-02-02 15:16:32 +0530

    causesalesApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React, { Component } from 'react';
 import './App.css';
-import Usurvey from './FireBase/Usurvey';
+import Coursesales from './Causesales';
 
-
-function App() {
-  return (
-    <div className="App">
-     <p>Welcome  </p>
-     <Usurvey/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    var courses = [
+      { name: 'Complete iOS10 dev course', price: 199 },
+      { name: 'Complete pentesting course', price: 299 },
+      { name: 'Complete front end dev course', price: 180 },
+      { name: 'Bug Bounty and web app pentesting', price: 190 }
+    ];
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Course Purchase Page</h2>
+        </div>
+        <Coursesales items={courses}/>
+      </div>
+    );
+  }
 }
 
 export default App;
