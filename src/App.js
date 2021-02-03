e0d66a0deaa6e9c3737d57ca2d5f77c9f42647f1
Bhagya <bhagya@fot.ruh.ac.lk>
2021-02-03 16:28:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,110 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import React,{Component} from "react";
-import Github from './Github'
-import Header from './Components/Header'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Auth0Lock from 'auth0-lock'
+import Github from './Github';
+import Header from './Components/Header';
+import Auth0Lock from 'auth0-lock';
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            idToken:'',
-            profile: {}
-        };
-    }
 
-    static defaultProps = {
-        clientID:'83h8S8tAD0GkxTNCDp6Val7RMK1rxcxf',
-        domain:'dev-b7ey3t-4.au.auth0.com'
-    }
-    componentWillMount() {
-    this.lock = new Auth0Lock(this.props.clientID,this.props.domain);
-    this.lock.on("authenticated",(authResult)=>{
-        console.log(authResult);
+  constructor(props){
+    super(props);
+
+    this.state = {
+      idToken: '',
+      profile: {}
+    };
+  }
+
+  static defaultProps = {
+    clientID: '',
+    domain: ''
+  }
+
+  componentWillMount(){
+    this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
+
+    this.lock.on('authenticated', (authResult) => {
+      // console.log(authResult);
+
+      this.lock.getProfile(authResult.idToken, (error, profile) => {
+        if(error){
+          console.log(error);
+          return;
+        }
+        // console.log(profile);
+
+        this.setProfile(authResult.idToken, profile);
+
+      });
+
     });
-    }
-    showLock(){
-        this.lock.show();
-    }
-    render() {
+    this.getProfile();
+  }
 
-        return (
-            <div className="App">
-                <Header onLogin={this.showLock.bind(this)}/>
-                <Github/>
-            </div>
-        );
+  setProfile(idToken, profile){
+    localStorage.setItem('idToken', idToken);
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    this.setState({
+      idToken: localStorage.getItem('idToken'),
+      profile: JSON.parse(localStorage.getItem('profile'))
+    });
+  }
+
+  getProfile(){
+    if(localStorage.getItem('idToken') != null){
+      this.setState({
+        idToken: localStorage.getItem('idToken'),
+        profile: JSON.parse(localStorage.getItem('profile'))
+      }, () => {
+        console.log(this.state);
+      });
     }
+  }
+
+
+  showLock(){
+    this.lock.show();
+  }
+
+
+  logout(){
+    this.setState({
+      idToken: '',
+      profile: ''
+    }, () => {
+      localStorage.removeItem('idToken');
+      localStorage.removeItem('profile');
+
+    });
+  }
+
+
+
+  render() {
+    let gitty;
+
+    if(this.state.idToken){
+      gitty = <Github />
+    } else{
+      gitty = "Click on Login to view Github Viewer"
+    }
+
+    return (
+      <div className="App">
+        <Header
+          lock={this.lock}
+          idToken={this.state.idToken}
+          onLogout={this.logout.bind(this)}
+          onLogin={this.showLock.bind(this)}
+           />
+         {gitty}
+      </div>
+    );
+  }
 }
+
 export default App;
