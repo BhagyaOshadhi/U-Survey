2555823aef9a042eac967c9545622124d217482f
Bhagya <bhagya@fot.ruh.ac.lk>
2021-03-16 16:13:29 +0530

    dateTimepicker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-
-import './App.css';
+import React, {Fragment, useState } from 'react';
+import DatePickerR from 'react-datetime';
+import moment from 'moment';
+import 'react-datetime/css/react-datetime.css';
+import { DatePicker } from "@material-ui/pickers";
+import { DateTimePicker, KeyboardDateTimePicker } from "@material-ui/pickers";
 
 function App(props) {
+  const [dt, setDt] = useState(moment());
+    const [selectedDate, handleDateChange] = useState(new Date("2018-01-01T00:00:00.000Z"));
   return(
-    <div>
-      welcome
-      <button>hi</button>
-    </div>
+      <Fragment>
+      <div className="App"  style={{marginLeft:"40%"}} >
+        <h2>Date Time Picker</h2>
+          {/*<DateTimePicker*/}
+          {/*    variant="inline"*/}
+          {/*    label="Basic example"*/}
+          {/*    value={selectedDate}*/}
+          {/*    onChange={handleDateChange}*/}
+          {/*/>*/}
+         <DatePickerR
+            inputProps={{
+              style: { width: 250 }
+            }}
+            value={dt}
+            dateFormat="DD-MM-YYYY"
+            timeFormat="hh:mm A"
+            onChange={val => setDt(val)}
+        />
+        <br />
+        <div><b>Date:</b> {dt.format('LLL')}</div>
+           </div>
+        </Fragment>
   );
 };
 
