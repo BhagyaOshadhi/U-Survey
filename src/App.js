c2fcdab7d5264f9ac10cfa34499cf3c69e0d2727
Bhagya <bhagya@fot.ruh.ac.lk>
2021-05-22 11:15:20 +0530

    dynamic form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
+import React,{useState} from 'react';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
 
-import './App.css';
+const useStyles = makeStyles((theme)=>({
+  root:{
+   '& .MuiTextField-root' : {
+    //  background: 'red',
+     margin: theme.spacing(1),
+   },
+   
+  },button:{ margin: theme.spacing(1),}
+
+}))
+
 
 function App(props) {
+  const classes = useStyles();
+  const [inputFields,setInputFields] = useState([
+    {firtsName:"", lastName:""}
+  ])
+  const handleChangeInput=(index,event)=>{
+    console.log(index, event.target.value)
+  }
   return(
-    <div>
-      welcome
-      <button>hi</button>
-    </div>
+    <Container>
+      <h1>Add new User</h1>
+      <form className={classes.root}>
+        {inputFields.map((inputField,index)=>(
+          <div key= {index}>
+            <TextField 
+              name="FirstName"
+              label="First Name"
+              variant="filled"
+              value={inputField.firtsName}
+              onChange ={(event)=>handleChangeInput(index,event)}
+            />
+             <TextField 
+              name="LastName"
+              label="Last Name"
+              variant="filled"
+              value={inputField.lastName}
+              onChange ={(event)=>handleChangeInput(index,event)}
+            />
+            <IconButton><RemoveIcon/> </IconButton>
+            <IconButton><AddIcon/> </IconButton>
+          </div>
+        ))}
+        <Button className={classes.button} variant="contained" color="primary" type="submit" endIcon={<Icon>send</Icon>}>Send</Button>
+      </form>
+
+    </Container>
   );
 };
 
