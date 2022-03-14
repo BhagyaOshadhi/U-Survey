8d3eb2ec372e045da745b3057451e9350f74ce23
Oshadhi Bhagya <oshadi@Hatchyard.io>
2022-03-14 15:14:20 +0530

    chat app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 
 function App(props) {
-  return(
-    <div>
-      welcome
-      <button>hi</button>
-    </div>
-  );
-};
+  return <ChatEngine 
+  height="100vh"
+  projectID="47f95c8a-d22a-47a6-a951-82b143b343f2"
+  userName=""
+  userSecret = ""
+  />;
+}
 
 export default App;
